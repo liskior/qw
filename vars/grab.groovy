4d3f9f85ad6abd52752a8d3fe1b75d55ca546305
liskior <liskior@gmail.com>
2018-12-04 21:33:29 +0100

    almost the laaast

--- vars/grab.groovy
+++ vars/grab.groovy
@@ -2,6 +2,6 @@ package vars
 
 @GrabResolver(name = 'com.gotomeeting.builds', root = 'https://artifactory.prodwest.citrixsaassbe.net/artifactory/libs-snapshot-local/')
 @Grab(group='com.gotomeeting.builds', module='jirasupport', version='1.0.32-SNAPSHOT')
-@Grab(group='com.gotomeeting.builds', module='sonatypesupport', version='1.0-SNAPSHOT')
+@Grab(group='com.gotomeeting.builds', module='sonatypesupport', version='1.0.1-SNAPSHOT')
 
 def call() {}
