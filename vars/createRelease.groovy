d924f2ed07609ebcc11f1f216145e7d4a1ff04db
liskior <liskior@gmail.com>
2018-12-04 19:47:32 +0100

    init

--- vars/createRelease.groovy
+++ vars/createRelease.groovy
@@ -3,7 +3,7 @@ package vars
 import com.gotomeeting.jirasupport.*
 
 @NonCPS
-def createRelease(oldReleaseId) {
+def call(oldReleaseId) {
     JiraSupport jiraSupport = new JiraSupport(ACCESS_USR, ACCESS_PSW, JIRA_URL)
     RefactorItem.refactorReleaseName(jiraSupport, oldReleaseId, "test rename")
     releaseId = CreateRelease.createRelease(jiraSupport, project_key, project_key + " NEXT RELEASE" + Math.random(), description, "2018-09-15")
