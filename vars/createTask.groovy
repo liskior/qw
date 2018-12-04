d0a9719ab40baeddf89e4134013a8f164e6aa65f
liskior <liskior@gmail.com>
2018-12-04 18:59:21 +0100

    init

--- vars/createTask.groovy
+++ vars/createTask.groovy
@@ -2,7 +2,7 @@ package vars
 
 @GrabResolver(name = 'com.gotomeeting.builds', root = 'https://artifactory.prodwest.citrixsaassbe.net/artifactory/libs-snapshot-local/')
 @Grab(group='com.gotomeeting.builds', module='jirasupport', version='1.0.30-SNAPSHOT')
-import com.gotomeeting.jirasupport.JiraSupport
+import com.gotomeeting.jirasupport.*
 
 
 @NonCPS
