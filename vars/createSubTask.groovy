c70dfb24a1a169184d3addd10e9eec53f31dda42
liskior <liskior@gmail.com>
2018-12-04 21:21:33 +0100

    almost the laaast

--- vars/createSubTask.groovy
+++ vars/createSubTask.groovy
@@ -5,7 +5,7 @@ import com.gotomeeting.jirasupport.*
 def call(release_key) {
     JiraSupport jiraSupport = new JiraSupport(ACCESS_USR, ACCESS_PSW, JIRA_URL)
     key = CreateIssue.createIssue(jiraSupport, project_key, env.STAGE_NAME, "bla", release_key)
-    echo "Subtask link:"
+    echo env.STAGE_NAME + "Subtask link:"
     echo IssueInfo.getIssueLink(jiraSupport, key)
     return key
 
