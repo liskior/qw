5c024227d54a21ceff844c7b44476a3c22009c37
liskior <liskior@gmail.com>
2018-12-04 20:32:44 +0100

    init

--- vars/releaseRelease.groovy
+++ vars/releaseRelease.groovy
@@ -6,7 +6,7 @@ import com.gotomeeting.jirasupport.*
 @NonCPS
 def call(release_id) {
     JiraSupport jiraSupport = new JiraSupport(ACCESS_USR, ACCESS_PSW, JIRA_URL)
-    ReleaseRelease.releaseRelease(jiraSupport, project_key, release_id)
+    ReleaseRelease.releaseRelease(jiraSupport, project_key, release_id.toString())
 }
 
 
